ee46cc6a8815d86bbe1b16e56a672be139a3ac2c
Jignesh Patel <9671042+Jignesh-UI@users.noreply.github.com>
2018-05-21 19:45:41 +0530

    Add files via upload

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PermissionService } from 'app/userControls/permission.service';
 
 @Component({
   moduleId: module.id,
@@ -7,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  isLoading;
+  pagePermission = ['admin', 'view', 'full', 'home', 'reservation', 'setup', 'add', 'jignesh'];
 
-  constructor() { }
+  constructor(private resetPermission: PermissionService) { }
 
   ngOnInit() {
+    this.resetPermission.resetPermissions(this.pagePermission);
   }
 
 }
